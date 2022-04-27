c15dda936b39b4be586ae17a7a299bcdac9ad6e8
barrier <chengfu.yang@lkcoffee.com>
2022-04-27 17:34:46 +0800

    fix: webpack.config.dev.js

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -27,78 +27,39 @@ const devConfig = merge(mpa, {
       ]
     },
   },
+  module: {
+    rules: [
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   include: /node_modules/,
+      //   loader: 'babel-loader',
+      //   options: {
+      //     plugins: [
+      //       // 解决 Uncaught ReferenceError: regeneratorRuntime is not defined 报错
+      //       // 使用webpack-dev-server时，.babelrc的配置不生效，所以在这里再配置使之生效
+      //       '@babel/plugin-transform-runtime'
+      //     ]
+      //   }
+      // },
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   exclude: path.resolve(__dirname, '../src/views/'),
+      //   loader: 'babel-loader',
+      //   options: {
+      //     plugins: [
+      //       [
+      //         'import',
+      //         {
+      //           'libraryName': 'vant',
+      //           'libraryDirectory': 'es',
+      //           'style': true
+      //         }
+      //       ],
+      //     ]
+      //   }
+      // },
+    ]
+  }
 })
-devConfig.module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      include: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        plugins: [
-          // 解决 Uncaught ReferenceError: regeneratorRuntime is not defined 报错
-          // 使用webpack-dev-server时，.babelrc的配置不生效，所以在这里再配置使之生效
-          '@babel/plugin-transform-runtime'
-        ]
-      }
-    },
-    // {
-    //   test: /\.(js|jsx)$/,
-    //   exclude: path.resolve(__dirname, '../src/views/'),
-    //   loader: 'babel-loader',
-    //   options: {
-    //     plugins: [
-    //       [
-    //         'import',
-    //         {
-    //           'libraryName': 'vant',
-    //           'libraryDirectory': 'es',
-    //           'style': true
-    //         }
-    //       ],
-    //     ]
-    //   }
-    // },
-    {
-      test: /\.(png|jpe?g|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    },
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      options: {
-        loaders: {
-          js: [
-            { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } }
-          ],
-          less: [
-            { loader: 'postcss-loader' }
-          ],
-        }
-      }
-    },
-    {
-      test: /\.css$/i,
-      use: [
-        'style-loader',
-        'css-loader',
-        'postcss-loader'
-      ]
-    },
-    {
-      test: /\.less$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'less-loader',
-        'postcss-loader'
-      ]
-    },
-  ]
-}
 
 module.exports = devConfig
